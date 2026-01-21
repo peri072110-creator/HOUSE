69effb34e1020454390709726929bfc6f815871a
agzet <agzetjik@gmail.com>
2026-01-21 10:36:27 +0600

    admin registered

--- house_app/admin.py
+++ house_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import *
+admin.site.register(UserProfile)
+admin.site.register(Region)
+admin.site.register(City)
+admin.site.register(District)
+admin.site.register(Property)
+admin.site.register(PropertyImage)
+admin.site.register(PropertyDocument)
+admin.site.register(Review)
